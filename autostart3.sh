a2b6ff8d241dd36899377f4257989df37bbb2b92
fanyang <314043711@qq.com>
2018-03-18 21:04:47 +0800

    delete useless files

--- autostart3.sh
+++ autostart3.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-xte 'key Return'
-xte 'usleep 100000'
-xte 'key Return'
-xte 'usleep 100000'
-xte 'key Up'
-xte 'usleep 100000'
-xte 'key Up'
-xte 'usleep 100000'
-xte 'key Return'#/usr/bin/bash
+#/usr/bin/bash
 xte 'key Return'
 xte 'usleep 100000'
 xte 'key Return'
