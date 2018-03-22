74109437067a620174fe1a6e9994930ded0ea96a
fanyang <314043711@qq.com>
2018-03-22 20:32:49 +0000

    way to use image as input

--- actor_network.py
+++ actor_network.py
@@ -1,7 +1,5 @@
-import tensorflow as tf 
-import numpy as np
+import tensorflow as tf
 import math
-
 
 # Hyper Parameters
 LAYER1_SIZE = 300
@@ -9,16 +7,18 @@ LAYER2_SIZE = 600
 LEARNING_RATE = 1e-4
 TAU = 0.001
 BATCH_SIZE = 32
+
 class ActorNetwork:
     """docstring for ActorNetwork"""
-    def __init__(self,sess,state_dim,action_dim):
+    def __init__(self, sess, state_dim, action_dim, img_dim):
 
         self.sess = sess
         self.state_dim  = state_dim
         self.action_dim = action_dim
         
-        self.state_input, self.action_output, self.net = self.create_network(state_dim, action_dim)
-        self.target_state_input, self.target_action_output, self.target_update, self.target_net = self.create_target_network(state_dim, action_dim, self.net)
+        self.img_input, self.state_input, self.action_output, self.net = self.create_network(state_dim, action_dim, img_dim)
+        self.target_img_input, self.target_state_input, self.target_action_output, \
+            self.target_update, self.target_net = self.create_target_network(state_dim, action_dim, self.net, img_dim)
 
         # define training rules
         self.create_training_method()
@@ -29,13 +29,8 @@ class ActorNetwork:
         #self.load_network()
 
     def create_training_method(self):
-        self.q_gradient_input = tf.placeholder("float", [None, self.action_dim])
+        self.q_gradient_input = tf.placeholder(dtype=tf.float32, shape=[None, self.action_dim])
         self.parameters_gradients = tf.gradients(self.action_output, self.net, -self.q_gradient_input)
-        '''        
-        for i, grad in enumerate(self.parameters_gradients):
-            if grad is not None:
-                self.parameters_gradients[i] = tf.clip_by_value(grad, -2.0,2.0)
-	    '''
         self.optimizer = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(list(zip(self.parameters_gradients, self.net)))
     
     def create_network(self, state_dim, action_dim, img_dim):
@@ -45,22 +40,52 @@ class ActorNetwork:
 
         # Image state input
         img_input = tf.placeholder(dtype=tf.uint8, shape=[None, img_dim[0], img_dim[1], img_dim[2]])
-        img_w1 = tf.Variable(tf.random_uniform([layer2_size, 1], -1e-4, 1e-4))
-        img_w1 = tf.nn.conv2d(img_input, , strides=[1, 1, 1, 1], padding='SAME')
+
+        img_w1 = tf.Variable(tf.random_uniform(([5, 5, 3, 16]), -1e-4, 1e-4))
+        img_b1 = tf.Variable(tf.random_uniform([16], 1e-4, 1e-4))
+        # 60*60*16
+        img_layer1 = tf.nn.relu(tf.nn.conv2d(img_input, img_w1, strides=[1, 1, 1, 1]) + img_b1)
+        # 30*30*16
+        img_layer2 = tf.nn.max_pool(img_layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+
+        img_w2 = tf.Variable(tf.random_uniform(([5, 5, 16, 32]), -1e-4, 1e-4))
+        img_b2 = tf.Variable(tf.random_uniform([32], 1e-4, 1e-4))
+        # 26*26*32
+        img_layer3 = tf.nn.relu(tf.nn.conv2d(img_layer2, img_w2, strides=[1, 1, 1, 1]) + img_b2)
+        # 13*13*32
+        img_layer4 = tf.nn.max_pool(img_layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+
+        img_w3 = tf.Variable(tf.random_uniform(([4, 4, 32, 64]), -1e-4, 1e-4))
+        img_b3 = tf.Variable(tf.random_uniform([64], 1e-4, 1e-4))
+        # 10*10*64
+        img_layer5 = tf.nn.relu(tf.nn.conv2d(img_layer4, img_w3, strides=[1, 1, 1, 1]) + img_b3)
+        # 5*5*64
+        img_layer6 = tf.nn.max_pool(img_layer5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+        img_layer7 = img_layer6.reshape(5*5*64)
+
+        img_w4 = tf.Variable(tf.random_uniform([5*5*64, layer1_size], -1e-4, 1e-4))
+        img_b4 = tf.Variable(tf.random_uniform([layer1_size]), -1e-4, 1e-4)
+
+        img_layer8 = tf.nn.relu(tf.matmul(img_layer7, img_w4) + img_b4)
 
         # Scalar state input
         state_input = tf.placeholder(dtype=tf.float32, shape=[None, state_dim])
         state_w1 = self.variable([state_dim, layer1_size], state_dim)
         state_b1 = self.variable([layer1_size], state_dim)
-        state_w2 = self.variable([layer1_size, layer2_size], layer1_size)
-        state_b2 = self.variable([layer2_size], layer1_size)
 
         layer1 = tf.nn.relu(tf.matmul(state_input, state_w1) + state_b1)
-        layer2 = tf.nn.relu(tf.matmul(layer1, state_w2) + state_b2)
+        layer2 = tf.add(img_layer8, layer1)
+
+        state_w2 = self.variable([layer1_size, layer2_size], layer1_size)
+        state_b2 = self.variable([layer2_size], layer1_size)
+        layer3 = tf.nn.relu(tf.matmul(layer2, state_w2) + state_b2)
 
         steer_w = tf.Variable(tf.random_uniform([layer2_size, 1], -1e-4, 1e-4))
         steer_b = tf.Variable(tf.random_uniform([1], -1e-4, 1e-4))
-        steer = tf.tanh(tf.matmul(layer2, steer_w) + steer_b)
+        steer = tf.tanh(tf.matmul(layer3, steer_w) + steer_b)
 
         # accel_w = tf.Variable(tf.random_uniform([layer2_size, 1], -1e-4, 1e-4))
         # accel_b = tf.Variable(tf.random_uniform([1], -1e-4, 1e-4))
@@ -72,67 +97,66 @@ class ActorNetwork:
         
         # action_output = tf.concat([steer, accel, brake], 1)
         action_output = steer
-        return state_input,action_output,[state_w1, state_b1, state_w2, state_b2, steer_w, steer_b]
+        return img_input, state_input, action_output, \
+               [state_w1, state_b1, state_w2, state_b2, steer_w, steer_b, img_w1, img_b1, img_w2, img_b2, img_w3, img_b3, img_w4, img_b4]
 
-    def create_target_network(self, state_dim, action_dim, net, use_bn=False):
+    def create_target_network(self, state_dim, action_dim, net, img_dim):
         state_input = tf.placeholder("float", [None, state_dim])
         ema = tf.train.ExponentialMovingAverage(decay=1-TAU)
         target_update = ema.apply(net)
         target_net = [ema.average(x) for x in net]
 
+        img_input = tf.placeholder(dtype=tf.float32, shape=[None, img_dim[0], img_dim[1], img_dim[2]])
+        img_layer1 = tf.nn.relu(tf.nn.conv2d(img_input, net[6], strides=[1, 1, 1, 1]) + net[7])
+        img_layer2 = tf.nn.max_pool(img_layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer3 = tf.nn.relu(tf.nn.conv2d(img_layer2, net[8], strides=[1, 1, 1, 1]) + net[9])
+        img_layer4 = tf.nn.max_pool(img_layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer5 = tf.nn.relu(tf.nn.conv2d(img_layer4, net[10], strides=[1, 1, 1, 1]) + net[11])
+        img_layer6 = tf.nn.max_pool(img_layer5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer7 = img_layer6.reshape(5*5*64)
+        img_layer8 = tf.nn.relu(tf.matmul(img_layer7, net[12]) + net[13])
         layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
-        layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + target_net[3])
-
-        steer = tf.tanh(tf.matmul(layer2, target_net[4]) + target_net[5])
+        layer2 = tf.add(img_layer8, layer1)
+        layer3 = tf.nn.relu(tf.matmul(layer2, target_net[2]) + target_net[3])
+        steer = tf.tanh(tf.matmul(layer3, target_net[4]) + target_net[5])
         # accel = tf.sigmoid(tf.matmul(layer2,target_net[6]) + target_net[7])
         # brake = tf.sigmoid(tf.matmul(layer2,target_net[8]) + target_net[9])
         # action_output = tf.concat([steer, accel, brake], 1)
 
         action_output = steer
-        return state_input, action_output, target_update, target_net
+        return img_input, state_input, action_output, target_update, target_net
 
     def update_target(self):
         self.sess.run(self. target_update)
 
-    def train(self, q_gradient_batch, state_batch):
+    def train(self, q_gradient_batch, state_batch, img_batch):
         self.sess.run(self.optimizer, feed_dict={
             self.q_gradient_input : q_gradient_batch,
-            self.state_input : state_batch
+            self.state_input : state_batch,
+            self.img_input : img_batch
             })
 
-    def actions(self, state_batch):
+    def actions(self, state_batch, img_batch):
         return self.sess.run(self.action_output, feed_dict={
-            self.state_input : state_batch
+            self.state_input : state_batch,
+            self.img_input : img_batch
             })
 
-    def action(self, state):
+    def action(self, state, img):
         return self.sess.run(self.action_output, feed_dict={
-            self.state_input : [state]
+            self.state_input : [state],
+            self.img_input : [img]
             })[0]
 
 
-    def target_actions(self,state_batch):
+    def target_actions(self, state_batch, img_batch):
         return self.sess.run(self.target_action_output, feed_dict={
-            self.target_state_input:state_batch
+            self.target_state_input : state_batch,
+            self.target_img_input : img_batch
             })
 
     # f fan-in size
     def variable(self, shape, f):
         return tf.Variable(tf.random_uniform(shape, -1/math.sqrt(f), 1/math.sqrt(f)))
-    '''
-    def load_network(self):
-        self.saver = tf.train.Saver()
-        checkpoint = tf.train.get_checkpoint_state("saved_actor_networks")
-        if checkpoint and checkpoint.model_checkpoint_path:
-            self.saver.restore(self.sess, checkpoint.model_checkpoint_path)
-            print "Successfully loaded:", checkpoint.model_checkpoint_path
-        else:
-            print "Could not find old network weights"
-            
-    def save_network(self,time_step):
-        print 'save actor-network...',time_step
-        self.saver.save(self.sess, 'saved_actor_networks/' + 'actor-network', global_step = time_step)
-
-    '''
 
 
--- critic_network.py
+++ critic_network.py
@@ -11,14 +11,14 @@ TAU = 0.001
 L2 = 0.0001
 
 class CriticNetwork:
-    def __init__(self, sess, state_dim, action_dim):
+    def __init__(self, sess, state_dim, action_dim, img_dim):
         self.time_step = 0
         self.sess = sess
 
-        self.state_input, self.action_input, self.q_value_output, self.net = self.create_q_network(state_dim, action_dim)
+        self.img_input, self.state_input, self.action_input, self.q_value_output, self.net = self.create_q_network(state_dim, action_dim, img_dim)
 
-        self.target_state_input, self.target_action_input, self.target_q_value_output, self.target_update = \
-            self.create_target_q_network(state_dim, action_dim, self.net)
+        self.target_img_input, self.target_state_input, self.target_action_input, self.target_q_value_output, self.target_update = \
+            self.create_target_q_network(state_dim, action_dim, self.net, img_dim)
 
         self.create_training_method()
 
@@ -29,18 +29,49 @@ class CriticNetwork:
 
     def create_training_method(self):
         # Define training optimizer
-        self.y_input = tf.placeholder("float", [None, 1])
+        self.y_input = tf.placeholder(dtype=tf.float32, shape=[None, 1])
         weight_decay = tf.add_n([L2 * tf.nn.l2_loss(var) for var in self.net])
         self.cost = tf.reduce_mean(tf.square(self.y_input - self.q_value_output)) + weight_decay
         self.optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(self.cost)
         self.action_gradients = tf.gradients(self.q_value_output, self.action_input)
 
-    def create_q_network(self,state_dim,action_dim):
+    def create_q_network(self, state_dim, action_dim, img_dim):
         layer1_size = LAYER1_SIZE
         layer2_size = LAYER2_SIZE
 
-        state_input = tf.placeholder("float",[None, state_dim])
-        action_input = tf.placeholder("float",[None, action_dim])
+        # Image state input
+        img_input = tf.placeholder(dtype=tf.uint8, shape=[None, img_dim[0], img_dim[1], img_dim[2]])
+
+        img_w1 = tf.Variable(tf.random_uniform(([5, 5, 3, 16]), -1e-4, 1e-4))
+        img_b1 = tf.Variable(tf.random_uniform([16], 1e-4, 1e-4))
+        # 60*60*16
+        img_layer1 = tf.nn.relu(tf.nn.conv2d(img_input, img_w1, strides=[1, 1, 1, 1]) + img_b1)
+        # 30*30*16
+        img_layer2 = tf.nn.max_pool(img_layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+        img_w2 = tf.Variable(tf.random_uniform(([5, 5, 16, 32]), -1e-4, 1e-4))
+        img_b2 = tf.Variable(tf.random_uniform([32], 1e-4, 1e-4))
+        # 26*26*32
+        img_layer3 = tf.nn.relu(tf.nn.conv2d(img_layer2, img_w2, strides=[1, 1, 1, 1]) + img_b2)
+        # 13*13*32
+        img_layer4 = tf.nn.max_pool(img_layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+        img_w3 = tf.Variable(tf.random_uniform(([4, 4, 32, 64]), -1e-4, 1e-4))
+        img_b3 = tf.Variable(tf.random_uniform([64], 1e-4, 1e-4))
+        # 10*10*64
+        img_layer5 = tf.nn.relu(tf.nn.conv2d(img_layer4, img_w3, strides=[1, 1, 1, 1]) + img_b3)
+        # 5*5*64
+        img_layer6 = tf.nn.max_pool(img_layer5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+
+        img_layer7 = img_layer6.reshape(5*5*64)
+
+        img_w4 = tf.Variable(tf.random_uniform([5*5*64, layer1_size], -1e-4, 1e-4))
+        img_b4 = tf.Variable(tf.random_uniform([layer1_size]), -1e-4, 1e-4)
+
+        img_layer8 = tf.nn.relu(tf.matmul(img_layer7, img_w4) + img_b4)
+
+        state_input = tf.placeholder(dtype=tf.float32, shape=[None, state_dim])
+        action_input = tf.placeholder(dtype=tf.float32, shape=[None, action_dim])
 
         state_w1 = self.variable([state_dim, layer1_size], state_dim)
         state_b1 = self.variable([layer1_size], state_dim)
@@ -53,54 +84,71 @@ class CriticNetwork:
         b3 = tf.Variable(tf.random_uniform([1], -3e-3, 3e-3))
 
         layer1 = tf.nn.relu(tf.matmul(state_input, state_w1) + state_b1)
-        layer2 = tf.nn.relu(tf.matmul(layer1, state_w2) + tf.matmul(action_input, action_w2) + b2)
-        q_value_output = tf.identity(tf.matmul(layer2, w3) + b3)
+        layer2 = tf.add(layer1, img_layer8)
+        layer3 = tf.nn.relu(tf.matmul(layer2, state_w2) + tf.matmul(action_input, action_w2) + b2)
+        q_value_output = tf.identity(tf.matmul(layer3, w3) + b3)
 
-        return state_input, action_input, q_value_output, [state_w1, state_b1, state_w2, action_w2, b2, w3, b3]
+        return img_input, state_input, action_input, q_value_output, \
+               [state_w1, state_b1, state_w2, action_w2, b2, w3, b3, img_w1, img_b1, img_w2, img_b2, img_w3, img_b3, img_w4, img_b4]
 
-    def create_target_q_network(self, state_dim, action_dim, net):
+    def create_target_q_network(self, state_dim, action_dim, net, img_dim):
         
-        state_input = tf.placeholder("float", [None, state_dim])
-        action_input = tf.placeholder("float", [None, action_dim])
+        state_input = tf.placeholder(dtype=tf.float32, shape=[None, state_dim])
+        action_input = tf.placeholder(dtype=tf.float32, shape=[None, action_dim])
 
         ema = tf.train.ExponentialMovingAverage(decay=1-TAU)
         target_update = ema.apply(net)
         target_net = [ema.average(x) for x in net]
 
-        layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
-        layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + tf.matmul(action_input, target_net[3]) + target_net[4])
-        q_value_output = tf.identity(tf.matmul(layer2, target_net[5]) + target_net[6])
+        img_input = tf.placeholder(dtype=tf.float32, shape=[None, img_dim[0], img_dim[1], img_dim[2]])
+        img_layer1 = tf.nn.relu(tf.nn.conv2d(img_input, net[7], strides=[1, 1, 1, 1]) + net[8])
+        img_layer2 = tf.nn.max_pool(img_layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer3 = tf.nn.relu(tf.nn.conv2d(img_layer2, net[9], strides=[1, 1, 1, 1]) + net[10])
+        img_layer4 = tf.nn.max_pool(img_layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer5 = tf.nn.relu(tf.nn.conv2d(img_layer4, net[11], strides=[1, 1, 1, 1]) + net[12])
+        img_layer6 = tf.nn.max_pool(img_layer5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+        img_layer7 = img_layer6.reshape(5*5*64)
+        img_layer8 = tf.nn.relu(tf.matmul(img_layer7, net[13]) + net[14])
 
-        return state_input, action_input, q_value_output, target_update
+        layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
+        layer2 = layer1 + img_layer8
+        layer3 = tf.nn.relu(tf.matmul(layer2, target_net[2]) + tf.matmul(action_input, target_net[3]) + target_net[4])
+        q_value_output = tf.identity(tf.matmul(layer3, target_net[5]) + target_net[6])
+
+        return img_input, state_input, action_input, q_value_output, target_update
 
     def update_target(self):
         self.sess.run(self.target_update)
 
-    def train(self, y_batch, state_batch, action_batch):
+    def train(self, y_batch, state_batch, action_batch, img_batch):
         self.time_step += 1
         cost, _ = self.sess.run([self.cost, self.optimizer], feed_dict={
             self.y_input : y_batch,
             self.state_input : state_batch,
-            self.action_input : action_batch
+            self.action_input : action_batch,
+            self.img_input : img_batch
         })
         return cost
 
-    def gradients(self, state_batch, action_batch):
+    def gradients(self, state_batch, action_batch, img_batch):
         return self.sess.run(self.action_gradients, feed_dict={
             self.state_input : state_batch,
-            self.action_input : action_batch
+            self.action_input : action_batch,
+            self.img_input : img_batch
         })[0]
 
-    def target_q(self, state_batch, action_batch):
+    def target_q(self, state_batch, action_batch, img_batch):
         return self.sess.run(self.target_q_value_output, feed_dict={
             self.target_state_input : state_batch,
-            self.target_action_input : action_batch
+            self.target_action_input : action_batch,
+            self.target_img_input : img_batch
         })
 
-    def q_value(self, state_batch, action_batch):
+    def q_value(self, state_batch, action_batch, img_batch):
         return self.sess.run(self.q_value_output, feed_dict={
             self.state_input : state_batch,
-            self.action_input : action_batch
+            self.action_input : action_batch,
+            self.img_input : img_batch
         })
 
     # f fan-in size
